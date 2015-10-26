64075f07aaa89d29fabba62e2189f6ed37360f63
Leo Allen <leo.f.allen@gmail.com>
2015-10-26 15:56:17 +0000

    Add dishes to an order

--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -2,11 +2,21 @@ require "menu"
 
 describe Menu do
 
-  subject(:menu) { described_class.new([dish]) }
+  subject(:menu) { described_class.new(dishes) }
 
-  let(:dish) { double("Dish", price: 1) }
+  let(:dishes) do
+    {
+      chicken: 3.99,
+      falafel: 4.50
+    }
+  end
 
-  it "shows a list of dishes with prices" do
-    expect(menu.dishes).to include(dish)
+  it "has a list of dishes with prices" do
+    expect(menu.dishes).to eq(dishes)
+  end
+
+  it "prints a list of dishes with prices" do
+    printed_menu = "Chicken £3.99, Falafel £4.50"
+    expect(menu.print).to eq(printed_menu)
   end
 end
---
+++ spec/order_spec.rb
@@ -0,0 +1,35 @@
+require "order"
+
+describe Order do
+  subject(:order) { described_class.new(menu) }
+
+  let(:menu) { double(:menu) }
+
+  let(:dishes) do
+    {
+      chicken: 2,
+      fish: 1
+    }
+  end
+
+  before do
+    allow(menu).to receive(:price).with(:chicken).and_return(3.00)
+    allow(menu).to receive(:price).with(:fish).and_return(2.50)
+  end
+
+  it "selects several dishes from the menu" do
+    create_order
+    expect(order.dishes).to eq(dishes)
+  end
+
+  it "calculates the total for the order" do
+    create_order
+    total = 8.50
+    expect(order.total).to eq(total)
+  end
+
+  def create_order
+    order.add(:chicken, 2)
+    order.add(:fish, 1)
+  end
+end
